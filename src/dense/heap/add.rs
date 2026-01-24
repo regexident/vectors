031beb2fb15b583c1f07c5915bbfd4efbde9a395
Vincent Esche <regexident@gmail.com>
2026-01-24 22:16:11 +0100

    Fix operator implementations of `DenseVec<…>` and `SparseVec<…>`

--- src/dense/heap/add.rs
+++ src/dense/heap/add.rs
@@ -6,33 +6,29 @@ use std::ops::{Add, AddAssign};
 
 use super::DenseVector;
 
-impl<T, V> Add<V> for DenseVector<T>
+impl<'a, T> Add<&'a Self> for DenseVector<T>
 where
-    T: AddAssign<T>,
-    V: IntoIterator<Item = (usize, T)>,
-    <V as IntoIterator>::IntoIter: ExactSizeIterator,
+    T: AddAssign<&'a T>,
 {
     type Output = Self;
 
     #[inline]
-    fn add(mut self, rhs: V) -> Self::Output {
+    fn add(mut self, rhs: &'a Self) -> Self::Output {
         self.add_assign(rhs);
         self
     }
 }
 
-impl<T, V> AddAssign<V> for DenseVector<T>
+impl<'a, T> AddAssign<&'a Self> for DenseVector<T>
 where
-    T: AddAssign<T>,
-    V: IntoIterator<Item = (usize, T)>,
-    <V as IntoIterator>::IntoIter: ExactSizeIterator,
+    T: AddAssign<&'a T>,
 {
     #[inline]
-    fn add_assign(&mut self, rhs: V) {
-        let iter = rhs.into_iter();
+    fn add_assign(&mut self, rhs: &'a Self) {
+        let iter = rhs.components.iter();
         debug_assert_eq!(self.len(), iter.len());
-        for (lhs, (_, rhs)) in self.components.iter_mut().zip(iter) {
-            *lhs += rhs;
+        for (lhs, rhs) in self.components.iter_mut().zip(iter) {
+            lhs.add_assign(rhs);
         }
     }
 }
@@ -45,15 +41,6 @@ mod test {
 
     #[test]
     fn add() {
-        let subject = DenseVector::from(vec![0.0, 0.5, 1.0, 2.0, 3.0]);
-        let other = DenseVector::from(vec![2.0, 1.0, 0.0, -1.0, -2.0]);
-        let expected = DenseVector::from(vec![2.0, 1.5, 1.0, 1.0, 1.0]);
-        let result = subject + other;
-        expect!(result).to(be_equal_to(expected));
-    }
-
-    #[test]
-    fn add_ref() {
         let subject = DenseVector::from(vec![0.0, 0.5, 1.0, 2.0, 3.0]);
         let other = DenseVector::from(vec![2.0, 1.0, 0.0, -1.0, -2.0]);
         let expected = DenseVector::from(vec![2.0, 1.5, 1.0, 1.0, 1.0]);
--- src/dense/heap/mul_add.rs
+++ src/dense/heap/mul_add.rs
@@ -6,33 +6,29 @@ use num_traits::{MulAdd, MulAddAssign};
 
 use super::DenseVector;
 
-impl<T, V> MulAdd<T, V> for DenseVector<T>
+impl<'a, T> MulAdd<T, &'a Self> for DenseVector<T>
 where
     T: Copy + MulAddAssign<T, T>,
-    V: IntoIterator<Item = (usize, T)>,
-    <V as IntoIterator>::IntoIter: ExactSizeIterator,
 {
     type Output = Self;
 
     #[inline]
-    fn mul_add(mut self, a: T, b: V) -> Self::Output {
+    fn mul_add(mut self, a: T, b: &'a Self) -> Self::Output {
         self.mul_add_assign(a, b);
         self
     }
 }
 
-impl<T, V> MulAddAssign<T, V> for DenseVector<T>
+impl<'a, T> MulAddAssign<T, &'a Self> for DenseVector<T>
 where
     T: Copy + MulAddAssign<T, T>,
-    V: IntoIterator<Item = (usize, T)>,
-    <V as IntoIterator>::IntoIter: ExactSizeIterator,
 {
     #[inline]
-    fn mul_add_assign(&mut self, a: T, b: V) {
-        let iter = b.into_iter();
+    fn mul_add_assign(&mut self, a: T, b: &Self) {
+        let iter = b.components.iter();
         debug_assert_eq!(self.len(), iter.len());
-        for (lhs, (_, rhs)) in self.components.iter_mut().zip(iter) {
-            lhs.mul_add_assign(a, rhs);
+        for (lhs, rhs) in self.components.iter_mut().zip(iter) {
+            lhs.mul_add_assign(a, rhs.to_owned());
         }
     }
 }
@@ -41,19 +37,12 @@ where
 mod test {
     use super::*;
 
+    use num_traits::{MulAdd, MulAddAssign};
+
     use expectest::prelude::*;
 
     #[test]
     fn mul_add() {
-        let subject = DenseVector::from(vec![0.0, 0.5, 1.0, 2.0, 3.0]);
-        let other = DenseVector::from(vec![2.0, 1.0, 0.0, -1.0, -2.0]);
-        let expected = DenseVector::from(vec![4.0, 2.5, 1.0, 0.0, -1.0]);
-        let result = other.mul_add(2.0, subject);
-        expect!(result).to(be_equal_to(expected));
-    }
-
-    #[test]
-    fn mul_add_ref() {
         let subject = DenseVector::from(vec![0.0, 0.5, 1.0, 2.0, 3.0]);
         let other = DenseVector::from(vec![2.0, 1.0, 0.0, -1.0, -2.0]);
         let expected = DenseVector::from(vec![4.0, 2.5, 1.0, 0.0, -1.0]);
